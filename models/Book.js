b9a2a87146ec7f1d5b8056a1ccee5a9cec7f877e
atlasmoth <ebuka422@gmail.com>
2020-07-08 20:17:37 +0100

    building book schema and routes

---
+++ models/Book.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please include title"],
+      unique: [true, "Name must be unique"],
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    description: {
+      type: String,
+      max: 1000,
+    },
+    dimensions: {
+      width: {
+        type: Number,
+        min: 0,
+      },
+      height: {
+        type: Number,
+        min: 0,
+      },
+    },
+    genre: {
+      type: String,
+      enum: ["Education", "Entertainment", "Development"],
+      default: "Education",
+    },
+    isbn: {
+      type: String,
+      match: [
+        /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/,
+        "Please include valid ISBN number",
+      ],
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
