99b174f6a17e71244af44e0393a8d2f4d8e1df1c
atlasmoth <ebuka422@gmail.com>
2020-07-11 22:11:57 +0100

    final commit for today

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,30 @@
 import Link from "next/link";
-export default function Header() {
+export default function Header(props) {
   return (
-    <header className="main-header">
-      <div className="heading">
-        <h1>Akidie</h1>
-      </div>
-      <nav className="navigation">
-        <ul>
-          <li>
-            <Link href="/signup">
-              <a>Sign Up</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/signin">
-              <a>Sign In</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="main-header">
+        <div className="heading">
+          <h1>Akidie</h1>
+        </div>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <Link href="/signup">
+                <a>Sign Up</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/signin">
+                <a>Sign In</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {props.children}
+      <footer>
+        <h4>This is the footer</h4>
+      </footer>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,80 +11,82 @@ export default function Home() {
         <meta name="description" content="An akiddie clone." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <main>
-        <section className="first">
-          <div className="container">
-            <div className="display">
-              <img src="/logo.png" alt="logo" />
-              <h1>Akidie</h1>
-            </div>
-            <div className="lede">
-              <h2 className="underscore">Lorem Ipsum dolor sit amet</h2>
-            </div>
-            <div className="register">
-              <button>Register</button>
-            </div>
-          </div>
-        </section>
-        <section className="second">
-          <div className="container">
-            <div className="grid">
-              <div className="testimonials">
-                <h2 className="underscore">Testimonials</h2>
+      <Header>
+        <main>
+          <section className="first">
+            <div className="container">
+              <div className="display">
+                <img src="/logo.png" alt="logo" />
+                <h1>Akidie</h1>
               </div>
-              <div className="message">
-                <div className="message-heading">
-                  <img src="/demo.svg" alt="" />
-                  <h4>Dum Dum</h4>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-                  eos quis quaerat eveniet necessitatibus adipisci ex corrupti
-                  iusto quidem nihil labore dolores porro expedita velit
-                  aliquid, dolorem iure dolor aperiam!
-                </p>
+              <div className="lede">
+                <h2 className="underscore">Lorem Ipsum dolor sit amet</h2>
               </div>
-              <div className="message">
-                <div className="message-heading">
-                  <img src="/demo.svg" alt="" />
-                  <h4>Dum Dum</h4>
-                </div>
-                <p>
-                  Tempora eaque similique sapiente, perspiciatis, quae quos
-                  doloremque praesentium velit quibusdam sed illum error
-                  eligendi illo quam temporibus deserunt asperiores iste, minus
-                  molestiae quo. Aliquid maiores inventore animi quaerat fugiat.
-                </p>
-              </div>
-              <div className="message">
-                <div className="message-heading">
-                  <img src="/demo.svg" alt="" />
-                  <h4>Dum Dum</h4>
-                </div>
-                <p>
-                  Sequi vero quaerat, quis suscipit at delectus esse ipsam
-                  molestiae minus velit ea consectetur! Nesciunt dolore porro ad
-                  deleniti, error magnam harum quisquam distinctio officia eius
-                  id magni, pariatur dolorum!
-                </p>
-              </div>
-              <div className="message">
-                <div className="message-heading">
-                  <img src="/demo.svg" alt="" />
-                  <h4>Dum Dum</h4>
-                </div>
-                <p>
-                  Qui quidem laborum, soluta fuga, ea quis nemo mollitia
-                  eligendi ab delectus numquam, nulla animi sit ex. Aperiam amet
-                  sequi accusamus, architecto quos veniam animi, labore magnam
-                  eaque eos voluptatum!
-                </p>
+              <div className="register">
+                <button>Register</button>
               </div>
             </div>
-          </div>
-        </section>
-      </main>
+          </section>
+          <section className="second">
+            <div className="container">
+              <div className="grid">
+                <div className="testimonials">
+                  <h2 className="underscore">Testimonials</h2>
+                </div>
+                <div className="message">
+                  <div className="message-heading">
+                    <img src="/demo.svg" alt="" />
+                    <h4>Dum Dum</h4>
+                  </div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+                    eos quis quaerat eveniet necessitatibus adipisci ex corrupti
+                    iusto quidem nihil labore dolores porro expedita velit
+                    aliquid, dolorem iure dolor aperiam!
+                  </p>
+                </div>
+                <div className="message">
+                  <div className="message-heading">
+                    <img src="/demo.svg" alt="" />
+                    <h4>Dum Dum</h4>
+                  </div>
+                  <p>
+                    Tempora eaque similique sapiente, perspiciatis, quae quos
+                    doloremque praesentium velit quibusdam sed illum error
+                    eligendi illo quam temporibus deserunt asperiores iste,
+                    minus molestiae quo. Aliquid maiores inventore animi quaerat
+                    fugiat.
+                  </p>
+                </div>
+                <div className="message">
+                  <div className="message-heading">
+                    <img src="/demo.svg" alt="" />
+                    <h4>Dum Dum</h4>
+                  </div>
+                  <p>
+                    Sequi vero quaerat, quis suscipit at delectus esse ipsam
+                    molestiae minus velit ea consectetur! Nesciunt dolore porro
+                    ad deleniti, error magnam harum quisquam distinctio officia
+                    eius id magni, pariatur dolorum!
+                  </p>
+                </div>
+                <div className="message">
+                  <div className="message-heading">
+                    <img src="/demo.svg" alt="" />
+                    <h4>Dum Dum</h4>
+                  </div>
+                  <p>
+                    Qui quidem laborum, soluta fuga, ea quis nemo mollitia
+                    eligendi ab delectus numquam, nulla animi sit ex. Aperiam
+                    amet sequi accusamus, architecto quos veniam animi, labore
+                    magnam eaque eos voluptatum!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </Header>
     </div>
   );
 }
