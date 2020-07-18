e72ace14030aa71ff9891ebf13b3ef2053ea6b30
atlasmoth <ebuka422@gmail.com>
2020-07-18 22:56:38 +0100

    last features for today

--- pages/read/[read].js
+++ pages/read/[read].js
@@ -1,8 +1,32 @@
 import Header from "../../components/Header";
 import Cookies from "js-cookie";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Read(props) {
+  const [state, setState] = useState({ data: undefined, loading: true });
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
   useEffect(() => {
     fetch(`http://localhost:3000/books/5f1202010b64ff1374114ece/read`, {
       method: "GET",
@@ -14,23 +38,59 @@ export default function Read(props) {
         if (res.ok && res.status === 200) {
           return res.blob();
         } else {
-          return res.json();
+          throw res.json();
         }
       })
-      .then((data) => {
-        const reader = new FileReader();
-        reader.onload = function (stuff) {
-          console.log(stuff);
-          console.log(reader.result);
-        };
-        reader.readAsDataURL(data);
+      .then(async (data) => {
+        toBase(data)
+          .then((res) => {
+            setState((s) => {
+              return { ...s, data: res, loading: false };
+            });
+          })
+          .catch(console.log);
       })
       .catch(console.log);
   }, []);
+
   return (
     <Header>
-      <div className="container">
-        <h2>I love to read</h2>
+      <div className="pdf-container">
+        {state.data && (
+          <>
+            <Document
+              file={state.data}
+              onLoadError={console.error}
+              onLoadSuccess={onDocumentLoadSuccess}
+              width="400"
+            >
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <div className="slider">
+              <p>
+                Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+              </p>
+              <div className="slide-buttons">
+                <button
+                  type="button"
+                  disabled={pageNumber <= 1}
+                  onClick={previousPage}
+                  className="read"
+                >
+                  Previous
+                </button>
+                <button
+                  className="read"
+                  type="button"
+                  disabled={pageNumber >= numPages}
+                  onClick={nextPage}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Header>
   );
@@ -42,4 +102,15 @@ export async function getServerSideProps() {
       beans: "man, i love beans",
     },
   };
+}
+
+function toBase(streamObj) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function () {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(streamObj);
+  });
 }
